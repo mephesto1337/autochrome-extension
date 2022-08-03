d6cc80dc936cb9a40cb07939f9209701871955bd
Thomas WACHE <thomas@wache.fr>
2022-08-03 14:54:50 +0200

    Initial commit

---
+++ generate-profile-rules.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+cd "$(dirname "${0}")"
+
+declare -a COLORS=(
+    'blue'
+    'cyan'
+    'green'
+    'orange'
+    'purple'
+    'red'
+    'white'
+    'yellow'
+)
+
+for color in ${COLORS[@]}; do
+    sed -re "s/\\\$\\{COLOR\\}/${color}/g" useragent_tag_tpl.json > useragent_tag_${color}.json
+done
+
+exit 0
