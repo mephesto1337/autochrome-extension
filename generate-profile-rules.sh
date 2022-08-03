da7324db39bd109f50d66645ae07155f9d8963bf
Thomas WACHE <thomas@wache.fr>
2022-08-03 15:11:13 +0200

    Some files reorganization

--- generate-profile-rules.sh
+++ generate-profile-rules.sh
@@ -14,7 +14,7 @@ declare -a COLORS=(
 )
 
 for color in ${COLORS[@]}; do
-    sed -re "s/\\\$\\{COLOR\\}/${color}/g" useragent_tag_tpl.json > useragent_tag_${color}.json
+    sed -re "s/\\\$\\{COLOR\\}/${color}/g" useragent_tag_tpl.json > src/useragent_tag_${color}.json
 done
 
 exit 0
